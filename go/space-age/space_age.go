9d8b64c6c2b60e4c73ad957e2b4a083b2ef81be7
Connie Leung <cleung334@gmail.com>
2017-12-05 22:57:03 +0800

    Complete space age problem in go

---
+++ go/space-age/space_age.go
@@ -0,0 +1,16 @@
+package space
+
+const EarthSecPerYear = 365.25 * 24 * 60 * 60
+
+func Age(seconds float64, planet string) float64 {
+	m := make(map[string]float64)
+	m["Earth"] = EarthSecPerYear
+	m["Mercury"] = 0.2408467 * EarthSecPerYear
+	m["Venus"] = 0.61519726 * EarthSecPerYear
+	m["Mars"] = 1.8808158 * EarthSecPerYear
+	m["Jupiter"] = 11.862615 * EarthSecPerYear
+	m["Saturn"] = 29.447498 * EarthSecPerYear
+	m["Uranus"] = 84.016846 * EarthSecPerYear
+	m["Neptune"] = 164.79132 * EarthSecPerYear
+	return float64(seconds) / m[planet]
+}
