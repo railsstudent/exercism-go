ecf7271eee39722932c7592d69fe67a7b744bf8b
Connie Leung <cleung334@gmail.com>
2017-12-07 22:39:57 +0800

    code refactoring

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -6,8 +6,8 @@ func Abbreviate(s string) string {
 	var acm string
 	for _, word := range strings.Split(s, " ") {
 		for _, token := range strings.Split(word, "-") {
-			acm += strings.ToUpper(string(token[0]))
+			acm += string(token[0])
 		}
 	}
-	return acm
+	return strings.ToUpper(acm)
 }
