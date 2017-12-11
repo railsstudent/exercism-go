380f90a38a458eab673eece73e7e1d3690ed6b87
Connie Leung <cleung334@gmail.com>
2017-12-12 00:54:24 +0800

    scrabble scores

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,28 @@
+package scrabble
+
+import "strings"
+
+// Score computes scrabble score of a given word
+func Score(word string) int {
+	var score int
+	upperWord := strings.ToUpper(word)
+	for _, c := range upperWord {
+		switch c {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score++
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
+	}
+	return score
+}
