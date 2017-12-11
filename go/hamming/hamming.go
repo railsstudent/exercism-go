8847eea4c287250cb4802a461141171a7fea5e3b
Connie Leung <cleung334@gmail.com>
2017-12-12 00:06:11 +0800

    code refactoring

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,14 +4,12 @@ import "errors"
 
 // Distance calculates hamming distance
 func Distance(a, b string) (int, error) {
-	lenA, lenB := len(a), len(b)
-
-	if lenA != lenB {
+	if len(a) != len(b) {
 		return -1, errors.New("distance mismatch")
 	}
 	dist := 0
-	for i := 0; i < lenA; i++ {
-		if a[i] != b[i] {
+	for i, c := range a {
+		if c != rune(b[i]) {
 			dist++
 		}
 	}
