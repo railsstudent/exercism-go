b17538c0753a02b852f12e679954ba32c265d4f7
Connie Leung <cleung334@gmail.com>
2018-01-10 00:31:22 +0800

    WIP

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -1,5 +1,38 @@
 package matrix
 
-func New(strMatrix string) {
+import "fmt"
+import "strings"
 
+type Matrix struct {
+	rows [][]int
+	cols [][]int
+}
+
+func New(strMatrix string) (*Matrix, error) {
+	strRows := strings.Split(strMatrix, "\n")
+	rows := [][]int{}
+	for _, strRow := range strRows {
+		elements := strings.Split(strRow, " ")
+		for _, e := range elements {
+			fmt.Println(int(e))
+		}
+	}
+
+	fmt.Println(strMatrix)
+	fmt.Println()
+
+	m := &Matrix{rows: [][]int{[]int{1}}, cols: [][]int{}}
+	return m, nil
+}
+
+func (m *Matrix) Cols() [][]int {
+	return [][]int{}
+}
+
+func (m *Matrix) Rows() [][]int {
+	return [][]int{}
+}
+
+func (m *Matrix) Set(r int, col int, val int) bool {
+	return false
 }
